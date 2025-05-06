28c3bfa9a928d6fd755873b423ae45a4abb089a7
Netaphor Dev <support@netapho.net>
2025-05-06 13:59:05 +0100

    Add charts

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,40 @@ const logger = require("firebase-functions/logger");
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = onRequest((request, response) => {
-    logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello from Firebase!");
+const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path');
+const dayjs = require('dayjs');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files (optional)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Example route
+app.get('/', (req, res) => {
+    const today = dayjs();
+    const days = [];
+    const data = {};
+    for (let i = 0; i < 3; i++) {
+        days.push(today.add(i, 'day').format('dddd'));
+    }
+    
+    const turnpoints = ['AFB', 'DDK', 'GDE'];
+    
+    data.days = days;
+    data.turnpoints = turnpoints;
+    // Render the EJS template
+    res.render('index', { 
+        title: 'Paragliding Dashboard',
+        days: days,
+        turnpoints: turnpoints
+    });
 });
+
+// Export as a Firebase function
+exports.app = functions.https.onRequest(app);
